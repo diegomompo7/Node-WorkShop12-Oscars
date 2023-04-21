1c2f897a6254c257fe36e3d7084eab3e932dc863
Diego Mompó Redoli <diego.mompo.redoli@gmail.com>
2023-04-21 20:07:52 +0200

    NodeWorkShop12Oscars

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Hola!");
+const express = require("express");
+const fs = require("fs");
+
+const PORT = 3000;
+const server  = express()
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    fs.readFile("./template/home.html", "utf-8", (readError, data) => {
+        res.set("Content-Type", "text/html");
+        res.send(data)
+    })
+})
+
+router.get("/oscars", (req, res) => {
+    fs.readdir("./data", "utf-8", (readError, data) => {
+        if (readError)
+        console.log(readError);
+        else{
+        res.set("Content-Type", "text/html");
+        res.send(data)
+        }
+    })
+})
+
+
+server.use("/", router);
+
+server.listen(PORT, () => {
+  console.log(`Servidor está levantado y escuchando en el puerto ${PORT}`);
+});
